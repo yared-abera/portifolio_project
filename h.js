8cfb200659fd2ab92e06f50def617199902c8867
yared-abera <yaredoabera7@gmail.com>
2024-05-20 00:41:30 +0300

    humberg menu

---
+++ h.js
@@ -0,0 +1,8 @@
+function showmenubar() {
+    const news = document.querySelector('.menu_toolbar')
+    news.style.display = 'flex'
+}
+function hidemenubar() {
+    const news = document.querySelector('.menu_toolbar')
+    news.style.display = 'none'
+}
