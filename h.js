9c38d70399360a645c4ce0f3bb35284244321df5
yared-abera <yaredoabera7@gmail.com>
2024-05-20 23:04:20 +0300

    task 7

--- h.js
+++
@@ -1,8 +0,0 @@
-function showmenubar() {
-    const news = document.querySelector('.menu_toolbar')
-    news.style.display = 'flex'
-}
-function hidemenubar() {
-    const news = document.querySelector('.menu_toolbar')
-    news.style.display = 'none'
-}
